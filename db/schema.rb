514bc924bd19e4bf74a1ee5fe4825e87779fe6bf
Kunal Chaudhari <kunalchaudhari@gmail.com>
2015-04-24 10:40:53 +0530

    geolocation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424041925) do
+ActiveRecord::Schema.define(version: 20150424045109) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150424041925) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer  "category_id"
+    t.string   "location_name"
   end
 
   create_table "users", force: :cascade do |t|
